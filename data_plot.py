df29d4e13792d0caf7f1b152a077662d8d0bf66d
ridiculoys <lltalip@up.edu.ph>
2022-10-07 16:19:58 +0800

    added jupyter notebook for plotting data

--- data_plot.py
+++ data_plot.py
@@ -8,6 +8,19 @@ def load_data(filename):
 
     return df
 
+def plot_data(title, df):
+    data = pd.DataFrame({
+        'p1': df.loc[:, "p1_win"].tolist(),
+        'p2': df.loc[:, "p2_win"].tolist(),
+        'draw': df.loc[:, "draw"].tolist()
+    }, index=df.loc[:, "iter"].tolist())
+
+    print(data)
+
+    lines = data.plot.line()
+
+
+
 random_v_random = load_data("1_random_v_random.txt")
 q_v_random = load_data("2_q_v_random.txt")
 trained_v_random = load_data("3_trained_v_random.txt")
@@ -16,3 +29,11 @@ q_v_trained = load_data("4_q_v_trained.txt")
 data = [random_v_random, q_v_random, trained_v_random, q_v_trained]
 for df in data:
     print(df)
+
+    # plot_data("Random vs Random", random_v_random)
+
+df = pd.DataFrame({
+   'pig': [20, 18, 489, 675, 1776],
+   'horse': [4, 25, 281, 600, 1900]
+   }, index=[1990, 1997, 2003, 2009, 2014])
+lines = df.plot.line()
