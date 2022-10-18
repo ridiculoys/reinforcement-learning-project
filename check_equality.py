6e190321a5d8a759b4d9d0f652e5268e82a3c391
ridiculoys <lltalip@up.edu.ph>
2022-10-18 17:06:15 +0800

    added logic to check the results of the policy evaluation algo

--- check_equality.py
+++ check_equality.py
@@ -7,9 +7,21 @@ def load_file(filename):
     print("Successfully loaded!")
     return q_table
 
+def print_dict(name, dic):
+    print(name)
+    count = 0
+    for key,value in dic.items():
+        print(f"key: {key}, value: {value}")
+        print()
+        count += 1
+        if count > 10:
+            break
 
-random = load_file("against_random/new_q_student_policy_20000")
-trained = load_file("against_trained/q_student_policy_20000")
-# print('ra', random)
-# print('tr', trained)
-print(random == trained)
+
+# random = load_file("against_random/new_q_student_policy_20000")
+# trained = load_file("against_trained/q_student_policy_20000")
+random = load_file("random_policy.txt")
+trained = load_file("new_trained_policy.txt")
+print_dict('RANDOM', random)
+print_dict('TRAINED', trained)
+# print(random == trained)
